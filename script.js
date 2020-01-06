245adf920b9c92bbfbebd3ff1fc0c039c77329a5
Ethan <esam5556@gmail.com>
2020-01-06 13:37:22 -0500

    Completed

--- script.js
+++ script.js
@@ -5,8 +5,7 @@
 
 
 
-6) Add the class main to the div with a class of footer.
-7) Remove the class main on the div with a class of footer.
+
 8) Create a new li element.
 9) Give the li the text "four".
 10) Append the li to the ul element.
@@ -35,5 +34,34 @@ console.log(listItem);
 containerElement.innerHTML+="Hello!";
 
 //6) Add the class main to the div with a class of footer.
+var addMain = document.getElementsByClassName("footer")[0];
+addMain.classList.add("main");
+console.log(addMain);
+
+//7) Remove the class main on the div with a class of footer.
+addMain.classList.remove("main");
+console.log(addMain);
+
+//8) Create a new li element.
+var newLi = document.createElement("li");
+
+//9) Give the li the text "four"
+newLi.innerHTML+="four";
+console.log(newLi);
+
+//10) Append the li to the ul element.
+document.getElementsByTagName("ul")[0].appendChild(newLi);
+
+
+//12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+var li = document.getElementsByTagName("ol")[0].children;
+
+for(let i = 0; i < li.length ; i++)
+{
+    li[i].style.backgroundColor ="green";
+}
+
+//13) Remove the div with a class of footer.
+addMain.parentNode.removeChild(addMain);
 
 
